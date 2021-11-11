3f98d57a4bb73c2cdd6c3258e15180be2c675946
r1tter <marcelosori@hotmail.com>
2021-11-11 12:19:11 -0300

    exemple

--- pages/api/restaurants.ts
+++ pages/api/restaurants.ts
@@ -3,6 +3,15 @@ import { NextApiResponse, NextApiRequest } from "next";
 
 const baseUrl = 'https://c8ad-187-19-91-113.ngrok.io';
 
+type Restaurant = {
+    id: string;
+    name: string;
+}
+
+type GetRestaurantResponse = {
+    restaurants: Restaurant[];
+}
+
 export default async function retrieveRestaurants (res: NextApiResponse, req: NextApiRequest) {
     try{
         const response = await axios.get(`${baseUrl}/restaurants/`);
@@ -11,3 +20,12 @@ export default async function retrieveRestaurants (res: NextApiResponse, req: Ne
         console.log(err)
     }
 }
+
+
+// export async function geRestaurants(): Promise<GetRestaurantResponse> {
+//     const {data, headers} = await axios.get(`${baseUrl}/restaurants`, {
+//         params : {
+            
+//         }
+//     })
+// }
